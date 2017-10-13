b31d5169192e876900ee78befcaa5b1122bf338c
Michael Andrew <michael.andrew@gladeye.com>
2017-10-13 14:04:51 +1300

    Tweaks to environment

--- app/components/aframe/land-component.js
+++ app/components/aframe/land-component.js
@@ -2,7 +2,8 @@ import AEntity from 'ember-aframe/components/a-entity';
 
 const LANDComponent = AEntity.extend({
     geometry:"primitive: box;",
-    material:"color: #0000ff; wireframe: true; wireframeLinewidth: 4;"
+    material:"color: #ffffff; wireframe: true; wireframeLinewidth: 4;",
+    shadow:""
 });
 
 LANDComponent.reopenClass({
