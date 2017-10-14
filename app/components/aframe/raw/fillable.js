7f75e3be2fb8002753acd8bc8001b33019b792d4
Michael Andrew <michael.andrew@gladeye.com>
2017-10-15 10:45:51 +1300

    Disabled debug logs

--- app/components/aframe/raw/fillable.js
+++ app/components/aframe/raw/fillable.js
@@ -2,14 +2,14 @@ import AFRAME from 'aframe';
 
 export default {
     init: function() {
-        console.log('inited');
+        //console.log('inited');
 
-        console.log('El: ',this.el);
+        //console.log('El: ',this.el);
 
         this.el.addEventListener('drag-drop', function(evt) {
 
-            console.log('Transformer el: ',this.el);
-            console.log('Dropped',evt.detail.dropped.parentNode);
+            //console.log('Transformer el: ',this.el);
+            //console.log('Dropped',evt.detail.dropped.parentNode);
 
             if(!(evt.detail.dropped.parentNode.getAttribute('class') === 'cube' && evt.detail.dropped.parentNode.getAttribute('template'))){
                 return;
@@ -17,7 +17,7 @@ export default {
 
 
 
-            console.log('Dropped template: ',evt.detail.dropped.parentNode.getAttribute('template'));
+            //console.log('Dropped template: ',evt.detail.dropped.parentNode.getAttribute('template'));
 
             //this.el.setAttribute('material',evt.detail.dropped.parentNode.getAttribute('material'));
             this.el.setAttribute('material','wireframe:false; transparent: true; opacity: 0');
