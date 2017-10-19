4811a04d5a859049dadb0c08c76990d82c7d0111
Michael Andrew <michael.andrew@gladeye.com>
2017-10-19 17:45:37 +1300

    Now only setting repeat property on `environment-ground-texture`

--- app/components/aframe/raw/environment-ground-texture.js
+++ app/components/aframe/raw/environment-ground-texture.js
@@ -1,63 +1,36 @@
 export default {
 
     schema: {
-        src:            { type: 'asset', required: true}, //this should be the URL to the texture file you wish to load. If loading cross-domain do NOT load it via a-asset-item as this will not send the correct headers
+        src:            { type: 'asset'}, //this should be the URL to the texture file you wish to load. If loading cross-domain do NOT load it via a-asset-item as this will not send the correct headers
         crossorigin:    { type: 'string', default: '' },//if the src and texture are loaded from a different domain, then set this to 'anonymous'
-        repeat:         { type: 'float', default: 1.0 } //number of times to repeat the texture across the material
+        repeat:         { type: 'float', default: 1.0 } //number of times to repeat the texture across the material. Affects new texture if src is set, otherwise the existing texture
     },
 
     init: function() {
 
         this.el.object3D.traverse(function(child){
 
-
-
             if(child instanceof THREE.Mesh){
 
                 if(child.geometry.type === "PlaneGeometry"){
-                    //is the floor Mesh
-                    console.log('Found floor: ', child);
 
-                    //var material = new THREE.MeshLambertMaterial()
-
-                    var loader = new THREE.TextureLoader();
-                    if (this.data.crossorigin) texloader.setCrossOrigin(self.data.crossorigin);
-
-                    console.log('Loading texture from: ',this.data.src.getAttribute('src'));
-
-                    loader.load(
-                        // resource URL
-                        this.data.src.getAttribute('src'),
-                        // Function when resource is loaded
-                        function ( texture ) {
-                            console.log('Loaded new texture: ',texture)
-
-                            texture.repeat.x = this.data.repeat;
-                            texture.repeat.y = this.data.repeat;
-                            //texture.repeat = new THREE.Vector2(10.1,10.1);
-                            texture.offset.set( 0, 0 );
-
-                            child.material.map = texture;
-                        }.bind(this)
-                    );
-
-
+                    if(this.data.src){
+                        var loader = new THREE.TextureLoader();
+                        if (this.data.crossorigin) texloader.setCrossOrigin(self.data.crossorigin);
+                        loader.load(
+                            this.data.src.getAttribute('src'),
+                            function ( texture ) {
+                                texture.repeat.x = this.data.repeat;
+                                texture.repeat.y = this.data.repeat;
+                                child.material.map = texture;
+                            }.bind(this)
+                        );
+                    }else{
+                        child.material.map.repeat.x = this.data.repeat;
+                        child.material.map.repeat.y = this.data.repeat;
+                    }
                 }
-
-                //child.geometry.mergeVertices();
-                //child.geometry.computeFaceNormals();
-                //child.geometry.computeVertexNormals();
-
-
-                //child.material.map = texture;
-                //child.material.needsUpdate = true;
-
-
-
-                //child.material = material;
-
             }
         }.bind(this));
-
     }
 };
