517cd987476b4ab7d89c8b5c242952a7e07664d6
Michael Andrew <michael.andrew@gladeye.com>
2017-10-27 11:22:03 +1300

    Improvements to colorize component

--- app/components/aframe/raw/colorize.js
+++ app/components/aframe/raw/colorize.js
@@ -1,31 +1,49 @@
 export default {
     schema: {
-        color: {type: 'color', default: 'white'}
+        color: {type: 'color', default: null}
     },
     init: function() {
-        this.el.addEventListener('componentinitialized',function(){
-            this.setColor(this.el);
-        }.bind(this));
-        this.el.addEventListener('child-attached',function(evt){
-            this.setColor(evt.detail.el);
-        }.bind(this));
+        this.setColor = this.setColor.bind(this);
+
+        var self = this;
+
+        self.el.addEventListener('componentinitialized',function(){
+            //console.log('componentinitialized, so setColor ', self.el);
+            self.setColor(self.el);
+        });
+
+        self.el.addEventListener('child-attached',function(evt){
+            //console.log('child-attached, so setColor ', evt.detail.el);
+            self.setColor(evt.detail.el);
+        });
     },
     update: function(){
-
-        console.log('Updated: ',this.data.color)
-
+        //console.log(this.el, ' Updated: ',this.data.color)
         this.setColor(this.el);
         this.updateChildren(this.el);
     },
     updateChildren: function(el){
         for(var i = 0; i < el.children.length; i++){
             this.setColor(el.children[i]);
-            if(el.children[i].children.length > 0){
+            if (el.children[i].children.length > 0) {
                 this.updateChildren(el.children[i]);
             }
         }
     },
     setColor: function(el){
-        el.setAttribute('material','color',this.data.color);
+        // Get current element color for comparison, or null.
+        var color = el.getAttribute('material') || null;
+        if (color) { color = color.color || null; }
+
+        if (this.data.color === null) {
+            //console.log(el, ' setColor null, ignoring and using ', color);
+            this.data.color = color; // this.el.setAttribute('colorize', 'color', color);
+        } else
+        if (this.data.color === color) {
+            //console.log(el, ' setColor, but already ', this.data.color);
+        } else {
+            //console.log(el, ' setColor from ', color, ' to ', this.data.color);
+            el.setAttribute('material', 'color', this.data.color);
+        }
     }
 };
